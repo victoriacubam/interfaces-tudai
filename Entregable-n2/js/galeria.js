189e46bbde6030019f56d31282461edea311eecf
mgutierrez <mgutierrez>
2023-10-05 19:43:41 -0300

    Se agrega galeria de imagenes

---
+++ Entregable-n2/js/galeria.js
@@ -0,0 +1,33 @@
+const carousel= document.querySelector(".carrucel2"),
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, prevPageX , prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth + 14;
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click" , () => {
+        carousel.scrollLeft += icon.id == "izq" ? -firstImgWidth : firstImgWidth;
+    })
+});
+
+const dragStart = (e) => {
+    isDragStart=true;
+    prevPageX= e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+ 
+const dragging = (e) => {
+    if(!isDragStart )return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff ;
+}
+
+const dragStop = () => {
+    isDragStart= false;
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
