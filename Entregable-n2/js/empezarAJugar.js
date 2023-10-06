9504fef33c3c31307ab17b56c24c4424d18f50c4
Victoria Cuba Martinez <victoriacuba01@gmail.com>
2023-10-06 20:31:22 -0300

    Agrega menu perfil y carrito

---
+++ Entregable-n2/js/empezarAJugar.js
@@ -0,0 +1,5 @@
+//Empezar a jugar
+let btnEmpezarJugar = document.querySelector("#empezar-jugar");
+btnEmpezarJugar.addEventListener("click", () => {
+         window.location.href = "juegoEnEjecucion.html" 
+});
