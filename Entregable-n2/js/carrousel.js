090adc5073c465d69b7eba61be82f3a0ef3743ce
mgutierrez <mgutierrez>
2023-10-06 11:46:44 -0300

    Se agrega carrousel de Recomendados

--- Entregable-n2/js/carrousel.js
+++ Entregable-n2/js/carrousel.js
@@ -1,33 +1,24 @@
-function App(){}
-    window.onload = function(event){
-        let app = new App();
-        window.app = app;
-    }
+const buttonPrev = document.getElementById('btn-prev');
+const buttonNext = document.getElementById('btn-next');
+const track = document.getElementById('track');
+const carrouselList = document.getElementById('carrousel-list');
+const card = document.querySelectorAll('.card');
 
-    App.prototype.processingButton= function(event){
-        constbtn = event.currentTarget;
-        const carrouselList = event.currentTargent.parentNode;
-        const track = event.currentTarget.parentNode.querySelector('#track');
-        const carrusel = track.querySelectorAll('.carrousel');
+const slickWidth = card[0].offsetWidth;
 
-        const carrouselWidth =  carrusel[0].offsetWidth;
-        
-        const trackWidth = track.offsetWidth;
-        const listWidth = carrouselList.offsetWidth;
+buttonPrev.onclick = () => Move(1);
+buttonNext.onclick = () => Move(2);
 
-        
-        track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = paserFloat(track.style.left.clice(0,-2) *-1);
-        btn.dataset.button = "izq" ? prevAction(leftPosition,carrouselWidth, track) : nextAction(leftPosition, trackWidth , listWidth, carrouselWidth, track);
-    }
+function Move(value) {
+    const trackWidth = track.offsetWidth;
+    const listWidth = carrouselList.offsetWidth;
 
-    let prevAction = (leftPosition, carrouselWidth , track) => {
-        if(leftPosition >0){
-            track.style.left=   `${-1 *(leftPosition - carrouselWidth)}px`
+    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0 , -2) * -1);
+
+    if(leftPosition < (trackWidth -listWidth) && value == 2){
+        track.style.left = `${-1 * (leftPosition + slickWidth)}px`
         }
-    }
-
-    let nextAction = (leftPosition , trackWidth , listWidth, carrouselWidth, track) =>{
-        if (leftPosition < (trackWidth = listWidth)){
-            track.style.left=   `${-1 *(leftPosition - carrouselWidth)}px`
+        else if(leftPosition > 0 && value == 1){
+            track.style.left =`${-1 * (leftPosition - slickWidth)}px`
         }
-    }
+}
