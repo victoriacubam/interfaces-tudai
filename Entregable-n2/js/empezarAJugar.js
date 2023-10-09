bc1252d783cc1f7774c6d8efbe16c9f1c27b713b
Victoria Cuba Martinez <victoriacuba01@gmail.com>
2023-10-09 01:11:02 -0300

    Ordena archivo css

--- Entregable-n2/js/empezarAJugar.js
+++ Entregable-n2/js/empezarAJugar.js
@@ -1,5 +1,5 @@
 //Empezar a jugar
-let btnEmpezarJugar = document.querySelector("#empezar-jugar");
+let btnEmpezarJugar = document.querySelector("#empezar-jugar-home");
 btnEmpezarJugar.addEventListener("click", () => {
          window.location.href = "juegoEnEjecucion.html" 
 });
