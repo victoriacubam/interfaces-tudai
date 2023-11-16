e6871e7dc6c06c5c0c9ca60852f38537fc191eff
Victoria <victoriacuba01@gmail.com>
2023-11-16 18:20:57 -0300

    Modifica seccion

---
+++ Entregable-n4/scroll.js
@@ -0,0 +1,13 @@
+let posPersonaje4Top = document.querySelector("#personaje-4").getBoundingClientRect().top;
+let posPersonaje4Bottom = document.querySelector("#personaje-4").getBoundingClientRect().bottom;
+
+
+window.onscroll = function(){
+    // console.log(posPersonaje4Top,window.scrollY, (window.scrollY + screen.height))
+    if ((window.scrollY >= posPersonaje4Top)&&(posPersonaje4Bottom>=window.scrollY)){
+        // console.log("hola");
+    }
+}
+// document.querySelector("#personaje-4").onscroll = function(){
+//     console.log("hola")
+// }
