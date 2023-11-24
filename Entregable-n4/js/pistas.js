c5ccb5bfb8e1758a8de06fa4a8f866124dcb5172
mgutierrez <mgutierrez>
2023-11-24 16:49:23 -0300

    Termino seccion 5

--- Entregable-n4/js/pistas.js
+++ Entregable-n4/js/pistas.js
@@ -1,19 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('scroll', function () {
-      let elementos = document.querySelectorAll('.pistas');
-  
-      elementos.forEach(function (elemento, index) {
-        let alturaElemento = elemento.offsetTop;
-        let alturaScroll = window.scrollY;
-        let windowHeight = window.innerHeight;
-  
-        if (alturaScroll > alturaElemento - windowHeight + 100) {
-          elemento.style.animation = 'aparecer 0.5s ease forwards ' + (index * 0.2) + 's';
-        } else {
-          // Si el elemento no es visible, eliminar la animación para que pueda reaparecer.
-          elemento.style.animation = '';
-        }
-      });
+  let elementos = document.querySelectorAll('.pistas');
+
+  window.addEventListener('scroll', function () {
+    elementos.forEach(function (elemento, index) {
+      let alturaElemento = elemento.getBoundingClientRect().top;
+      let alturaScroll = window.innerHeight;
+
+      if (alturaElemento < alturaScroll - 100) {
+        elemento.style.animation = 'aparecer 0.5s ease forwards ' + (index * 0.2) + 's';
+      } else {
+        elemento.style.animation = '';
+      }
     });
   });
-  
+
+  elementos.forEach(function (imagen) {
+    imagen.addEventListener('mouseenter', function () {
+      this.style.transition = 'transform 0.5s';
+      this.style.transform = 'perspective(1000px) rotateY(10deg)';
+    });
+
+    imagen.addEventListener('mouseleave', function () {
+      this.style.transition = 'transform 0.5s';
+      this.style.transform = 'perspective(1000px) rotateY(0deg)';
+    });
+  });
+});
--- Entregable-n4/js/vengadores.js
+++ Entregable-n4/js/vengadores.js
@@ -1,17 +1,28 @@
 
-const pantera = document.getElementById('capa5-Pantera');
+// const pantera = document.getElementById('capa5-Pantera');
 
-// Función para manejar el movimiento del mouse y el efecto de paralax
-function controlDesplazamiento(event) {
-    const mouseX = event.clientX; // Posición horizontal del mouse
-    const mouseY = event.clientY; // Posición vertical del mouse
+// // Función para manejar el movimiento del mouse y el efecto de paralax
+// function controlDesplazamiento(event) {
+//     const mouseX = event.clientX; // Posición horizontal del mouse
+//     const mouseY = event.clientY; // Posición vertical del mouse
 
-    // Calcula la cantidad de desplazamiento para la imagen de la Pantera
-    const offsetX = mouseX / window.innerWidth - 0.5;
-    const offsetY = mouseY / window.innerHeight - 0.5;
+//     // Calcula la cantidad de desplazamiento para la imagen de la Pantera
+//     const offsetX = mouseX / window.innerWidth - 0.5;
+//     const offsetY = mouseY / window.innerHeight - 0.5;
 
-    // Aplica el desplazamiento a la posición de la imagen
-    pantera.style.transform = `translate(${offsetX * 100}px, ${offsetY * 100}px)`;
-}
+//     // Aplica el desplazamiento a la posición de la imagen
+//     pantera.style.transform = `translate(${offsetX * 100}px, ${offsetY * 100}px)`;
+// }
 
-document.addEventListener('mousemove', controlDesplazamiento);
+// document.addEventListener('mousemove', controlDesplazamiento);
+
+
+document.addEventListener('mousemove', function (e) {
+    let parallaxElements = document.querySelectorAll('.parallax');
+    parallaxElements.forEach(function (element) {
+        let speed = parseFloat(element.getAttribute('data-speed'));
+        let x = (window.innerWidth - e.pageX * speed) / 100;
+        let y = (window.innerHeight - e.pageY * speed) / 100;
+        element.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+});
